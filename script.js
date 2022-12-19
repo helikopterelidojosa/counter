c787662d4f6675b3629a910479d706bdd311ee4d
codesoup <82330389+helikopterelidojosa@users.noreply.github.com>
2022-12-19 14:26:09 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,231 @@
+
+
+//variables
+var text1 = 0;
+var text2 = 0;
+var text3 = 0;
+var text4 = 0;
+
+var text5 = 0;
+var text6 = 0;
+var text7 = 0;
+var text8 = 0;
+
+var text9 = 0;
+var text10 = 0;
+var text11 = 0;
+var text12 = 0;
+
+var text13 = 0;
+var text14 = 0;
+var text15 = 0;
+var text16 = 0;
+
+
+//functions
+document.getElementById("ch1").addEventListener("click", counter1);
+document.getElementById("ch2").addEventListener("click", counter2);
+document.getElementById("ch3").addEventListener("click", counter3);
+document.getElementById("ch4").addEventListener("click", counter4);
+
+
+document.getElementById("ch5").addEventListener("click", counter5);
+document.getElementById("ch6").addEventListener("click", counter6);
+document.getElementById("ch7").addEventListener("click", counter7);
+document.getElementById("ch8").addEventListener("click", counter8);
+
+document.getElementById("ch9").addEventListener("click", counter9);
+document.getElementById("ch10").addEventListener("click", counter10);
+document.getElementById("ch11").addEventListener("click", counter11);
+document.getElementById("ch12").addEventListener("click", counter12);
+
+document.getElementById("ch13").addEventListener("click", counter13);
+document.getElementById("ch14").addEventListener("click", counter14);
+document.getElementById("ch15").addEventListener("click", counter15);
+document.getElementById("ch16").addEventListener("click", counter16);
+
+
+
+
+//1
+function counter1(){
+    if(text1 < 108){text1 = text1 +1;
+    }else{text1 = text1 + 0};
+     
+ document.getElementById('count1').innerHTML= text1;
+var krasasProcenti = text1 /108;
+
+ document.getElementById("ch1").style.backgroundColor = 'hsl('+(360*krasasProcenti)+',100%,50%)';
+}
+
+//2
+function counter2(){
+    if(text2 < 108){text2 = text2 +1;
+    }else{text2 = text2 + 0};
+     
+ document.getElementById('count2').innerHTML= text2;
+var krasasProcenti2 = text2 /108;
+
+ document.getElementById("ch2").style.backgroundColor = 'hsl('+(360*krasasProcenti2)+',100%,50%)';
+}
+
+//3
+function counter3(){
+    if(text3 < 108){text3 = text3 +1;
+    }else{text3 = text3 + 0};
+     
+ document.getElementById('count3').innerHTML= text3;
+var krasasProcenti3 = text3 /108;
+
+ document.getElementById("ch3").style.backgroundColor = 'hsl('+(360*krasasProcenti3)+',100%,50%)';
+}
+
+//4
+function counter4(){
+    if(text4 < 108){text4 = text4 +1;
+    }else{text4 = text4 + 0};
+     
+ document.getElementById('count4').innerHTML= text4;
+var krasasProcenti4 = text4 /108;
+
+ document.getElementById("ch4").style.backgroundColor = 'hsl('+(360*krasasProcenti4)+',100%,50%)';
+}
+
+//5
+function counter5(){
+    if(text5 < 108){text5 = text5 +1;
+    }else{text5 = text5 + 0};
+     
+ document.getElementById('count5').innerHTML= text5;
+var krasasProcenti5 = text5 /108;
+
+ document.getElementById("ch5").style.backgroundColor = 'hsl('+(360*krasasProcenti5)+',100%,50%)';
+}
+
+//6
+function counter6(){
+    if(text6 < 108){text6 = text6 +1;
+    }else{text6 = text6 + 0};
+     
+ document.getElementById('count6').innerHTML= text6;
+var krasasProcenti6 = text6 /108;
+
+ document.getElementById("ch6").style.backgroundColor = 'hsl('+(360*krasasProcenti6)+',100%,50%)';
+}
+
+//7
+function counter7(){
+    if(text7 < 108){text7 = text7 +1;
+    }else{text7 = text7 + 0};
+     
+ document.getElementById('count7').innerHTML= text7;
+var krasasProcenti7 = text7 /108;
+
+ document.getElementById("ch7").style.backgroundColor = 'hsl('+(360*krasasProcenti7)+',100%,50%)';
+}
+
+//8
+function counter8(){
+    if(text8 < 108){text8 = text8 +1;
+    }else{text8 = text8 + 0};
+     
+ document.getElementById('count8').innerHTML= text8;
+var krasasProcenti8 = text8 /108;
+
+ document.getElementById("ch8").style.backgroundColor = 'hsl('+(360*krasasProcenti8)+',100%,50%)';
+}
+
+//9
+function counter9(){
+    if(text9 < 108){text9 = text9 +1;
+    }else{text9 = text9 + 0};
+     
+ document.getElementById('count9').innerHTML= text9;
+var krasasProcenti9 = text9 /108;
+
+ document.getElementById("ch9").style.backgroundColor = 'hsl('+(360*krasasProcenti9)+',100%,50%)';
+}
+
+//10
+function counter10(){
+    if(text10 < 108){text10 = text10 +1;
+    }else{text10 = text10 + 0};
+     
+ document.getElementById('count10').innerHTML= text10;
+var krasasProcenti10 = text10 /108;
+
+ document.getElementById("ch10").style.backgroundColor = 'hsl('+(360*krasasProcenti10)+',100%,50%)';
+}
+
+//11
+function counter11(){
+    if(text11 < 108){text11 = text11 +1;
+    }else{text11 = text11 + 0};
+     
+ document.getElementById('count11').innerHTML= text11;
+var krasasProcenti11 = text11 /108;
+
+ document.getElementById("ch11").style.backgroundColor = 'hsl('+(360*krasasProcenti11)+',100%,50%)';
+}
+
+//12
+function counter12(){
+    if(text12 < 108){text12 = text12 +1;
+    }else{text12 = text12 + 0};
+     
+ document.getElementById('count12').innerHTML= text12;
+var krasasProcenti12 = text12 /108;
+
+ document.getElementById("ch12").style.backgroundColor = 'hsl('+(360*krasasProcenti12)+',100%,50%)';
+}
+
+//13
+function counter13(){
+    if(text13 < 108){text13 = text13 +1;
+    }else{text13 = text13 + 0};
+     
+ document.getElementById('count13').innerHTML= text13;
+var krasasProcenti13 = text13 /108;
+
+ document.getElementById("ch13").style.backgroundColor = 'hsl('+(360*krasasProcenti13)+',100%,50%)';
+}
+
+//14
+function counter14(){
+    if(text14 < 108){text14 = text14 +1;
+    }else{text14 = text14 + 0};
+     
+ document.getElementById('count14').innerHTML= text14;
+var krasasProcenti14 = text14 /108;
+
+ document.getElementById("ch14").style.backgroundColor = 'hsl('+(360*krasasProcenti14)+',100%,50%)';
+}
+
+//15
+function counter15(){
+    if(text15 < 108){text15 = text15 +1;
+    }else{text15 = text15 + 0};
+     
+ document.getElementById('count15').innerHTML= text15;
+var krasasProcenti15 = text15 /108;
+
+ document.getElementById("ch15").style.backgroundColor = 'hsl('+(360*krasasProcenti15)+',100%,50%)';
+}
+
+//16
+function counter16(){
+    if(text16 < 108){text16 = text16 +1;
+    }else{text16 = text16 + 0};
+     
+ document.getElementById('count16').innerHTML= text16;
+var krasasProcenti16 = text16 /108;
+
+ document.getElementById("ch16").style.backgroundColor = 'hsl('+(360*krasasProcenti16)+',100%,50%)';
+}
+
+
+function info(){
+var progress = Math.floor((text1+text2+text3+text4+text5+text6+text7+text8+text9+text10+text11+text12+text13+text14+text15+text16)/1728*100);
+ document.getElementById("infotext").innerHTML=progress+"%";
+}
+setInterval(info, 1000)
